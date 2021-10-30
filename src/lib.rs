8ad5b2fe58bb5d40e5d385b068cb3a5fb027b9f8
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-10-30 04:13:50 -0500

    update wasm_bindgen functions to return `JsValue`

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub use parser::*;
 pub use scanner::*;
 // pub use ream::*;
 
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -17,23 +19,42 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn ream2ast(source: &str) -> String {
-    let mut parser = Parser::new(&source);
-    let result = parser.parse_entry();
-    let output = result
-        .expect("a")
-        .expect("b")
-        .to_ast_str_pretty()
-        .expect("c");
+pub fn ream2ast(source: &str) -> Result<JsValue, JsValue> {
+    if source == "" {
+        return Ok(JsValue::NULL);
+    };
 
-    output
+    let mut parser = Parser::new(&source);
+
+    let ast: Result<JsValue, JsValue> = match parser.parse_entry() {
+        Err(e) => Err(JsValue::from_serde(&e).unwrap()),
+        Ok(opt_entry) => match opt_entry {
+            None => Ok(JsValue::NULL),
+            Some(entry) => Ok(JsValue::from_serde(&entry).unwrap()),
+        }
+    };
+
+   ast 
 }
 
 #[wasm_bindgen]
-pub fn ream2csv(source: &str) -> String {
-    let mut parser = Parser::new(&source);
-    let result = parser.parse_entry();
-    let output = result.expect("a").expect("b").to_csv_str().expect("c");
+pub fn ream2csv(source: &str) -> JsValue {
+    if source == "" {
+        return JsValue::NULL;
+    };
 
-    output
+    let mut parser = Parser::new(&source);
+
+    let result = match parser.parse_entry() {
+        Err(e) => JsValue::from_serde(&e).unwrap(),
+        Ok(opt_entry) => match opt_entry {
+            None => JsValue::NULL,
+            Some(entry) => {
+                let list = entry.to_csv_list().unwrap(); // TODO: unwrap?
+                JsValue::from_serde(&list).unwrap()
+            }
+        }
+    };
+
+    result
 }
